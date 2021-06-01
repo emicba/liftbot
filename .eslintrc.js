2096b5f95dfa839547bd47b253433d23cfdf9971
Emiliano Córdoba <emi.cbae@gmail.com>
2021-06-01 20:14:50 -0300

    :rotating_light: fix eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,22 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'prettier',
-    '@typescript-eslint',
-  ],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
     'import/extensions': 0,
     'import/no-unresolved': 0,
     'no-console': 0,
+    'no-underscore-dangle': 0,
+    'no-shadow': 0,
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': ['error'],
     'prettier/prettier': 'error',
   },
 };
