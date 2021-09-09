cfedf484644cdc02a0cb473f9771710001a785b5
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2021-09-09 19:07:41 -0300

    :recycle: add youtube search limit param

--- src/ytsearch.ts
+++ src/ytsearch.ts
@@ -1,14 +1,25 @@
+/* eslint-disable no-redeclare */
 import fetch from 'node-fetch';
 
-export default async function ytsearch(query: string): Promise<string | undefined> {
+async function ytsearch(query: string, limit?: 1): Promise<string>;
+async function ytsearch(query: string, limit: number): Promise<string[]>;
+async function ytsearch(query: string, limit: number = 1): Promise<string | string[]> {
   const response = await fetch(`https://www.youtube.com/results?search_query=${query}`);
-  if (response.ok) {
-    const body = await response.text();
-    const regex = /"videoRenderer":{"videoId":"(?<videoId>.{11})"/gm;
-    const videoId = regex.exec(body);
-    if (videoId?.groups?.videoId) {
-      return `https://youtu.be/${videoId?.groups?.videoId}`;
-    }
+  if (!response.ok) throw new Error('Could not fetch search results');
+  const body = await response.text();
+  const regex = /"videoRenderer":{"videoId":"(?<videoId>.{11})"/gm;
+
+  let match = regex.exec(body);
+  if (limit === 1 && match?.groups?.videoId) {
+    return `https://youtu.be/${match.groups.videoId}`;
   }
-  return undefined;
+
+  const results: string[] = [];
+  while (results.length < limit && match?.groups?.videoId) {
+    results.push(`https://youtu.be/${match.groups.videoId}`);
+    match = regex.exec(body);
+  }
+  return results;
 }
+
+export default ytsearch;
