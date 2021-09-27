87bd86e08ea777e70b861194f7146940ad28ac8f
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2021-09-27 00:03:38 -0300

    :bug: fix youtube search unescaped url

--- src/ytsearch.ts
+++ src/ytsearch.ts
@@ -3,7 +3,8 @@ import fetch from 'node-fetch';
 async function ytsearch(query: string, limit?: 1): Promise<string>;
 async function ytsearch(query: string, limit: number): Promise<string[]>;
 async function ytsearch(query: string, limit: number = 1): Promise<string | string[]> {
-  const response = await fetch(`https://www.youtube.com/results?search_query=${query}`);
+  const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
+  const response = await fetch(url);
   if (!response.ok) throw new Error('Could not fetch search results');
   const body = await response.text();
   const regex = /"videoRenderer":{"videoId":"(?<videoId>.{11})"/gm;
